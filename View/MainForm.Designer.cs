66ad72f6c321084ddedf69a060c497e26eaf0136
скда <@DESKTOP-9L2H0IN>
2024-02-16 15:51:46 +0700

    rrr

--- View/MainForm.Designer.cs
+++ View/MainForm.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             EnumsTabControl = new TabControl();
-            Enums = new TabPage();
-            EnumsListBox = new ListBox();
+            Enumeration = new TabPage();
+            label1 = new Label();
+            textBox1 = new TextBox();
             ValuesListBox = new ListBox();
+            EnumsListBox = new ListBox();
             EnumsTabControl.SuspendLayout();
-            Enums.SuspendLayout();
+            Enumeration.SuspendLayout();
             SuspendLayout();
             // 
             // EnumsTabControl
             // 
-            EnumsTabControl.Controls.Add(Enums);
+            EnumsTabControl.Controls.Add(Enumeration);
             EnumsTabControl.Dock = DockStyle.Fill;
             EnumsTabControl.Location = new Point(0, 0);
             EnumsTabControl.Name = "EnumsTabControl";
@@ -46,17 +48,43 @@
             EnumsTabControl.Size = new Size(800, 450);
             EnumsTabControl.TabIndex = 0;
             // 
-            // Enums
+            // Enumeration
             // 
-            Enums.Controls.Add(ValuesListBox);
-            Enums.Controls.Add(EnumsListBox);
-            Enums.Location = new Point(4, 29);
-            Enums.Name = "Enums";
-            Enums.Padding = new Padding(3);
-            Enums.Size = new Size(792, 417);
-            Enums.TabIndex = 0;
-            Enums.Text = "Enums";
-            Enums.UseVisualStyleBackColor = true;
+            Enumeration.Controls.Add(label1);
+            Enumeration.Controls.Add(textBox1);
+            Enumeration.Controls.Add(ValuesListBox);
+            Enumeration.Controls.Add(EnumsListBox);
+            Enumeration.Location = new Point(4, 29);
+            Enumeration.Name = "Enumeration";
+            Enumeration.Padding = new Padding(3);
+            Enumeration.Size = new Size(792, 417);
+            Enumeration.TabIndex = 0;
+            Enumeration.Text = "Enumeration";
+            Enumeration.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(597, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Int value:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(597, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // ValuesListBox
+            // 
+            ValuesListBox.FormattingEnabled = true;
+            ValuesListBox.Location = new Point(303, 35);
+            ValuesListBox.Name = "ValuesListBox";
+            ValuesListBox.Size = new Size(176, 304);
+            ValuesListBox.TabIndex = 1;
             // 
             // EnumsListBox
             // 
@@ -65,15 +93,6 @@
             EnumsListBox.Name = "EnumsListBox";
             EnumsListBox.Size = new Size(178, 304);
             EnumsListBox.TabIndex = 0;
-            EnumsListBox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
-            // 
-            // ValuesListBox
-            // 
-            ValuesListBox.FormattingEnabled = true;
-            ValuesListBox.Location = new Point(345, 35);
-            ValuesListBox.Name = "ValuesListBox";
-            ValuesListBox.Size = new Size(176, 304);
-            ValuesListBox.TabIndex = 1;
             // 
             // MainForm
             // 
@@ -84,15 +103,18 @@
             Name = "MainForm";
             Text = "Form1";
             EnumsTabControl.ResumeLayout(false);
-            Enums.ResumeLayout(false);
+            Enumeration.ResumeLayout(false);
+            Enumeration.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TabControl EnumsTabControl;
-        private TabPage Enums;
+        private TabPage Enumeration;
         private ListBox ValuesListBox;
         private ListBox EnumsListBox;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
